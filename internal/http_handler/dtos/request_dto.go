3185244c3c0c2f3ad65b7fc26b6a5b8175c98667
Bruno Lima <brunojet@gmail.com>
2025-06-30 10:25:40 -0300

    DTO

--- internal/http_handler/dtos/request_dto.go
+++ internal/http_handler/dtos/request_dto.go
@@ -10,8 +10,8 @@ type CreateSignRequestDTO struct {
 }
 
 type CreateSignResponseDTO struct {
-	ID           string `json:"id" binding:"required"`
-	PreSignedURL string `json:"pre_signed_url" binding:"required,url"`
+	ID        string `json:"id" binding:"required"`
+	UploadURL string `json:"pre_signed_url" binding:"required,url"`
 }
 
 func (d *CreateSignRequestDTO) GetDomainCreateSignRequest() *domain.SignRequest {
@@ -23,4 +23,8 @@ func (d *CreateSignRequestDTO) GetDomainCreateSignRequest() *domain.SignRequest 
 	}
 }
 
-type GetSignRequestDTO = domain.SignRequest
+type GetResponseDTO struct {
+	ID           string            `json:"id" binding:"required"`
+	SignerStatus domain.SignerStep `json:"signer_status,omitempty" binding:"required"`
+	DownloadURL  *string           `json:"pre_signed_url" binding:"required,url"`
+}
